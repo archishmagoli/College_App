6083b6ad0f053a7f49261e90161dd1c610c40119
archishmagoli <archishma.goli@gmail.com>
2021-03-03 18:06:17 -0500

    linked all pages

--- CollegeApp/App1/views.py
+++ CollegeApp/App1/views.py
@@ -7,3 +7,35 @@ def index(request):
 
 def login(request):
     return render(request, "login.html")
+
+def health(request):
+    return render(request, "health.html")
+
+def calendar(request):
+    return render(request, "calendar.html")
+
+def checklist(request):
+    return render(request, "checklist.html")
+
+def contacts(request):
+    return render(request, "contacts.html")
+
+def createaccount(request):
+    return render(request, "createaccount.html")
+
+def financial_aid(request):
+    return render(request, "financial_aid.html")
+
+def my_colleges(request):
+    return render(request, "my_colleges.html")
+
+def news(request):
+    return render(request, "news.html")
+
+def path2college(request):
+    return render(request, "path2college.html")
+
+def search(request):
+    return render(request, "search.html")
+
+
