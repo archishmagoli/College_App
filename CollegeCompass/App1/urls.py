d8cf0609170c6f00079f7a19d76ddef5fa007057
archishmagoli <archishma.goli@gmail.com>
2021-04-11 20:42:38 -0400

    create apps login, createaccount, index

---
+++ CollegeCompass/App1/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("health", views.health, name="health"),
+    path("calendar", views.calendar, name="calendar"),
+    path("checklist", views.checklist, name="checklist"),
+    path("contacts", views.contacts, name="contacts"),
+    path("financial_aid", views.financial_aid, name="financial_aid"),
+    path("search", views.search, name="search"),
+    path("my_colleges", views.my_colleges, name="my_colleges"),
+    path("news", views.news, name="news"),
+    path("path2college", views.path2college, name="path2college")
+]
