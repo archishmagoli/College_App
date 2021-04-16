ecda9e0bbc00c1fc79cb7b9328f9139ec8ebebb6
ananyar807 <ananyar807@gmail.com>
2021-04-16 19:36:00 -0400

    finished creating 2 apps and created apps for sophiya

--- CollegeCompass/App1/urls.py
+++ CollegeCompass/App1/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path("health", views.health, name="health"),
-    path("checklist", views.checklist, name="checklist"),
-    path("contacts", views.contacts, name="contacts"),
     path("financial_aid", views.financial_aid, name="financial_aid"),
     path("search", views.search, name="search"),
     path("my_colleges", views.my_colleges, name="my_colleges"),
